86ca568369f82f7dbf9909c2cf1eaa9936bcd126
Sofia Ansanelo <sofiaansanelo@gmail.com>
2024-03-18 09:30:30 -0300

    Aula sobre input e if

---
+++ Aula01/If.java
@@ -0,0 +1,18 @@
+package Aula01;
+
+class If {
+    public static void main(String[] args){
+        int x = 20;
+        int y = 18;
+
+        if (x > y){
+            System.out.println("x e maior que y");
+        } else if (x == y) {
+            System.out.println("x e y sao iguais");
+        } else {
+            System.out.println("y e maior que x");
+        }
+
+    }
+    
+}
---
+++ Aula01/imc.java
@@ -0,0 +1,30 @@
+package Aula01;
+
+import java.util.Scanner;
+
+class input{
+   public static void main(String[] args) {
+        Scanner peso = new Scanner(System.in);
+        Float peso1;
+        Scanner altura = new Scanner(System.in);
+        Float altura1;
+        Scanner nome = new Scanner(System.in);
+        String nome1;
+
+        System.out.println("Insira seu nome: ");
+        nome1 = nome.nextLine();
+
+        System.out.println("Insira seu peso: ");
+        peso1 = peso.nextFloat();
+
+        System.out.println("Insira sua altura: ");
+        altura1 = altura.nextFloat();
+
+        Float imc = (peso1) % (altura1 * altura1);
+
+        If (imc < 16,9 ) { 
+            System.out.println("");
+
+        }
+    
+}
