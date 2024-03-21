4134308949ecaa799e5b680dfedb273bfb5bc352
Sofia Ansanelo <sofiaansanelo@gmail.com>
2024-03-21 07:38:08 -0300

    atividade imc

--- Aula01/imc.java
+++ Aula01/imc.java
@@ -4,27 +4,37 @@ import java.util.Scanner;
 
 class input{
    public static void main(String[] args) {
-        Scanner peso = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Float peso1;
-        Scanner altura = new Scanner(System.in);
         Float altura1;
-        Scanner nome = new Scanner(System.in);
         String nome1;
 
         System.out.println("Insira seu nome: ");
-        nome1 = nome.nextLine();
+        nome1 = scanner.nextLine();
 
         System.out.println("Insira seu peso: ");
-        peso1 = peso.nextFloat();
+        peso1 = scanner.nextFloat();
 
         System.out.println("Insira sua altura: ");
-        altura1 = altura.nextFloat();
+        altura1 = scanner.nextFloat();
 
-        Float imc = (peso1) % (altura1 * altura1);
+        Float alturam = altura1/100.0f;
+        Float imc = (peso1) % (alturam * alturam);
 
-        If (imc < 16,9 ) { 
-            System.out.println(nome1, " tem " , altura1 , " de altura e pesa " , peso1 , "kg. Sua situação é MUITO ABAIXO DO PESO.");
-
-        } else if ()
-    
+        if (imc < 16.9) { 
+            System.out.println(nome1 + " tem " + alturam + " de altura e pesa " + peso1 + "kg. Sua situação é MUITO ABAIXO DO PESO.");
+        } else if (17 <= imc && imc <= 18.4) {
+            System.out.println(nome1 + " tem " + alturam + " de altura e pesa " + peso1 + "kg. Sua situação é Abaixo do peso");
+        } else if (18.5 <= imc && imc <= 24.9) {
+            System.out.println(nome1 + " tem " + alturam + " de altura e pesa " + peso1 + "kg. Sua situação é PESO NORMAL");
+        } else if (25 <= imc && imc <= 29.9) {
+            System.out.println(nome1 + " tem " + alturam + " de altura e pesa " + peso1 + "kg. Sua situação é ACIMA DO PESO");
+        } else if (30 <= imc && imc <= 34.9) {
+            System.out.println(nome1 + " tem " + alturam + " de altura e pesa " + peso1 + "kg. Sua situação é OBESIDADE GRAU I");
+        } else if (35 <= imc && imc <= 40) {
+            System.out.println(nome1 + " tem " + alturam + " de altura e pesa " + peso1 + "kg. Sua situação é OBESIDADE GRAU II");
+        } else if (imc < 40) {
+            System.out.println(nome1 + " tem " + alturam + " de altura e pesa " + peso1 + "kg. Sua situação é OBESIDADE GRAU III");
+        }
+   }
 }
