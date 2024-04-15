5aaff5eeef890e37a9a696b60847386deab8d22b
Sofia Ansanelo <sofiaansanelo@gmail.com>
2024-04-15 09:22:37 -0300

    laço de repetição

---
+++ aula03/Ex01.java
@@ -0,0 +1,9 @@
+public class Ex01 {
+    public static void main(String[] args) {
+        int i = 1;
+        int n = i + i;
+        int fib = i + n;
+  
+        }
+    }
+}
---
+++ aula03/Obj.java
@@ -0,0 +1,8 @@
+public class Obj {
+    public static void main(String[] args) {
+        int time = 20;
+        String result;
+        result = (time < 18) ? "Good day." : "Good evening";
+        System.out.print(result);
+    }
+}
