189d6e496390e150bbe4882fbb085e4b7bca835e
Sofia Ansanelo <sofiaansanelo@gmail.com>
2024-04-16 07:42:22 -0300

    laço de repetição

--- aula03/Ex01.java
+++ aula03/Ex01.java
@@ -1,9 +1,14 @@
 public class Ex01 {
     public static void main(String[] args) {
-        int i = 1;
-        int n = i + i;
-        int fib = i + n;
-  
+        int fibo = 1, n = 0;
+
+        System.out.println(n);
+        System.out.println(fibo);
+        for(int i = 0; i < 8; i++) {
+            fibo = fibo + n;
+            n = fibo - n;
+            System.out.println(fibo);
         }
+
     }
 }
