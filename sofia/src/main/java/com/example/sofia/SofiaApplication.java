64ed51cb17d38f3c32d1952ec5c1174117a49532
Sofia Ansanelo <sofiaansanelo@gmail.com>
2024-09-09 09:06:29 -0300

    restful

---
+++ sofia/src/main/java/com/example/sofia/SofiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sofia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofiaApplication.class, args);
+	}
+
+}
---
+++ sofia/src/test/java/com/example/sofia/SofiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sofia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
